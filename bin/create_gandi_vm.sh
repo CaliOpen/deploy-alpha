3c26c4fc0013290ea5800a40650c85d521cafc8e
Aymeric Barantal <mric@gandi.net>
2017-10-06 17:05:12 +0200

    Script to create platform on gandi and setup an host file

---
+++ bin/create_gandi_vm.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+# Vlan and its ipv4 gateway
+gandi vlan create --name alpha_vlan --datacenter FR-SD3 --subnet 192.168.1.0/24 --gateway 192.168.1.254
+gandi vm create --memory 1024 --hostname gw1 --image "Debian 8" --size 10G --datacenter FR-SD3 --vlan alpha_vlan --ip 192.168.1.254 --ip-version 4
+
+# Store machines
+gandi vm create --memory 3072 --hostname store1 --image "Debian 8" --size 50G --datacenter FR-SD3 --vlan alpha_vlan --ip 192.168.1.10 --ip-version 6
+gandi vm create --memory 3072 --hostname store2 --image "Debian 8" --size 50G --datacenter FR-SD3 --vlan alpha_vlan --ip 192.168.1.11 --ip-version 6
+gandi vm create --memory 3072 --hostname store3 --image "Debian 8" --size 50G --datacenter FR-SD3 --vlan alpha_vlan --ip 192.168.1.12 --ip-version 6
+gandi vm create --memory 3072 --hostname store4 --image "Debian 8" --size 50G --datacenter FR-SD3 --vlan alpha_vlan --ip 192.168.1.13 --ip-version 6
+gandi vm create --memory 3072 --hostname store5 --image "Debian 8" --size 50G --datacenter FR-SD3 --vlan alpha_vlan --ip 192.168.1.14 --ip-version 6
+
+# Cache
+gandi vm create --memory 1024 --hostname cache1 --image "Debian 8" --size 10G --datacenter FR-SD3 --vlan alpha_vlan --ip 192.168.1.50 --ip-version 6
+# object store
+gandi vm create --memory 1024 --hostname object-store1 --image "Debian 8" --size 50G --datacenter FR-SD3 --vlan alpha_vlan --ip 192.168.1.70 --ip-version 6
+# MQ
+gandi vm create --memory 1024 --hostname mq1 --image "Debian 8" --size 10G --datacenter FR-SD3 --vlan alpha_vlan --ip 192.168.1.60 --ip-version 6
+
+# Monitoring
+gandi vm create --memory 2048 --hostname prometheus1 --image "Debian 8" --size 50G --datacenter FR-SD3 --vlan alpha_vlan --ip 192.168.1.200 --ip-version 6
+gandi vm create --memory 1024 --hostname monitor1 --image "Debian 8" --size 50G --datacenter FR-SD3 --vlan alpha_vlan --ip 192.168.1.201 --ip-version 4
+
+# Api machines
+gandi vm create --memory 1024 --hostname apiv1-1 --image "Debian 8" --size 10G --datacenter FR-SD3 --vlan alpha_vlan --ip 192.168.1.100 --ip-version 4
+gandi vm create --memory 1024 --hostname apiv2-1 --image "Debian 8" --size 10G --datacenter FR-SD3 --vlan alpha_vlan --ip 192.168.1.110 --ip-version 4
+
+# LMTP
+gandi vm create --memory 1024 --hostname lmtp1 --image "Debian 8" --size 10G --datacenter FR-SD3 --vlan alpha_vlan --ip 192.168.1.20 --ip-version 6
---
+++ bin/make_hosts.sh
@@ -0,0 +1,19 @@
+#/bin/bash
+
+template_file='hosts.template'
+
+cp ${template_file} hosts.temp
+
+for machine in gw1 store1 store2 store3 store4 store5 cache1 mq1 object-store1 prometheus1 monitor1 apiv1-1 apiv2-1 lmtp1
+do
+	other_name=`echo ${machine}|tr "-" "_"`
+	echo "# Setting ${machine} (${other_name}) into inventory"
+	gandi vm info ${machine} > vm_info.tmp
+	ipv6=`grep ip6 vm_info.tmp |cut -d':' -f2-40|tr -d " " `
+	backend_ip=`grep ip4 vm_info.tmp |grep "192.168"|cut -d':' -f2-40|tr -d " " `
+	sed -i -e "s/ip_${other_name}/${ipv6}/g" hosts.temp
+	sed -i -e "s/backend_${other_name}/${backend_ip}/" hosts.temp
+	echo "ssh root@${ipv6}"
+done
+
+rm vm_info.tmp
