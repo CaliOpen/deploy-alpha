dd84c2bad3c3310d8f470a816e3978119096f3c2
Stan <git-ssa@sapienssapide.com>
2017-10-06 18:07:58 +0200

    Fix public net interface for apiV2-1 to bind ipv6 only.

--- bin/create_gandi_vm.sh
+++ bin/create_gandi_vm.sh
@@ -24,7 +24,7 @@ gandi vm create --memory 1024 --hostname monitor1 --image "Debian 8" --size 50G 
 
 # Api machines
 gandi vm create --memory 1024 --hostname apiv1-1 --image "Debian 8" --size 10G --datacenter FR-SD3 --vlan alpha_vlan --ip 192.168.1.100 --ip-version 4
-gandi vm create --memory 1024 --hostname apiv2-1 --image "Debian 8" --size 10G --datacenter FR-SD3 --vlan alpha_vlan --ip 192.168.1.110 --ip-version 4
+gandi vm create --memory 1024 --hostname apiv2-1 --image "Debian 8" --size 10G --datacenter FR-SD3 --vlan alpha_vlan --ip 192.168.1.110 --ip-version 6
 
 # LMTP
 gandi vm create --memory 1024 --hostname lmtp1 --image "Debian 8" --size 10G --datacenter FR-SD3 --vlan alpha_vlan --ip 192.168.1.20 --ip-version 6
