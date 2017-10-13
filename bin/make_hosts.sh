396d2f8f2a41e1cfc42767a56667011436434b78
Stan <git-ssa@sapienssapide.com>
2017-10-13 14:58:12 +0200

    Fix typo in make_hosts.sh and readme.

--- bin/make_hosts.sh
+++ bin/make_hosts.sh
@@ -2,17 +2,17 @@
 
 template_file='hosts.template'
 
-cp ${template_file} hosts.temp
+cp ${template_file} hosts
 
-for machine in gw1 store1 store2 store3 store4 store5 cache1 mq1 object-store1 prometheus1 monitor1 api1 api2 web-client1 lmtp1 worker1
+for machine in gw1 store1 store2 store3 store4 store5 cache1 mq1 object-store1 prometheus1 monitor1 api1 api2 web-client1 lmtp1 worker1 mail1
 do
 	other_name=`echo ${machine}|tr "-" "_"`
 	echo "# Setting ${machine} (${other_name}) into inventory"
 	gandi vm info ${machine} > vm_info.tmp
 	ipv6=`grep ip6 vm_info.tmp |cut -d':' -f2-40|tr -d " " `
 	backend_ip=`grep ip4 vm_info.tmp |grep "192.168"|cut -d':' -f2-40|tr -d " " `
-	sed -i -e "s/ip_${other_name}/${ipv6}/g" hosts.temp
-	sed -i -e "s/backend_${other_name}/${backend_ip}/" hosts.temp
+	sed -i -e "s/ip_${other_name}/${ipv6}/g" hosts
+	sed -i -e "s/backend_${other_name}/${backend_ip}/" hosts
 	echo "ssh root@${ipv6}"
 done
 
